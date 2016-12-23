fe26e65547c8fd254f84e20cd5aff7381fdd37ad
Finalcheat <finalcheat@qq.com>
2016-12-23 23:45:03 +0800

    add USER_AGENT and modify mobile01

--- news/settings.py
+++ news/settings.py
@@ -17,6 +17,7 @@ NEWSPIDER_MODULE = 'news.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'news (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS=32
--- news/spiders/mobile01.py
+++ news/spiders/mobile01.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import datetime
 from news.items import NewsItem
+from news.database import Database
 
 
 class Mobile01Spider(scrapy.Spider):
+    USE_PROXY = True
     name = "mobile01"
     allowed_domains = ["mobile01.com"]
     start_urls = (
@@ -13,14 +16,16 @@ class Mobile01Spider(scrapy.Spider):
 
     def parse(self, response):
         href = response.url
-        title = response.xpath('//header/h1[@class="topic"]/text').extract()[0]
-        pubtime = response.xpath('//div[@id="content"]/div[@id="section"]/div[@class="navbar"]/p[@class="nav"]/text()').extract()[0]
+        title = response.xpath('//header/h1[@class="topic"]/text()').extract()[0]
+        pubtime = response.xpath('//div[@id="content"]/div[@id="section"]/div[@class="navbar"]/p[@class="nav"][last()]/text()').extract()[0]
         r = re.compile(u'(\d+)-(\d+)-(\d+) (\d+):(\d+)')
         r = re.findall(r, pubtime)
         if not r:
             raise Exception(u'解析时间失败')
+        r = [ int(x) for x in r[0] ]
+        pubtime = datetime.datetime(year=r[0], month=r[1], day=r[2], hour=r[3], minute=r[4])
         htmlcontent = response.xpath('//div[@class="single-post"]/div[@class="single-post-content"]').extract()[0]
         keywords = []
         source = u'mobile01'
-        item = NewsItem(title=title, htmlcontent=htmlcontent, href=href, keywords=keywords, source=source)
+        item = NewsItem(title=title, pubtime=pubtime, htmlcontent=htmlcontent, href=href, keywords=keywords, source=source)
         yield item
