2c23468e9bc53fb4c208cadd36ace43a058b7f85
yaoshangdong <yaoshangdong@highlights.tw>
2016-12-26 15:04:59 +0800

    add letsgojp

--- news/spiders/letsgojp.py
+++ news/spiders/letsgojp.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import datetime
 from news.items import NewsItem
+from news.database import Database
 
 
 class LetsgojpSpider(scrapy.Spider):
+    USE_PROXY = True
     name = "letsgojp"
     allowed_domains = ["letsgojp.com"]
-    start_urls = (
-        'http://kyushu.letsgojp.com/archives/23471/',
-    )
+    # start_urls = (
+    #     'http://kyushu.letsgojp.com/archives/23471/',
+    # )
 
     def parse(self, response):
         href = response.url
@@ -19,9 +22,37 @@ class LetsgojpSpider(scrapy.Spider):
         r = re.findall(r, pubtime)
         if not r:
             raise Exception(u'解析时间失败')
+        r = [ int(x) for x in r[0] ]
+        pubtime = datetime.datetime(year=r[0], month=r[1], day=r[2])
         htmlcontent = response.xpath('//div[@id="postContent"]/div[@id="content"]').extract()[0]
-        print htmlcontent
         keywords = []
         source = u'日本樂吃購'
-        item = NewsItem(title=title, htmlcontent=htmlcontent, href=href, keywords=keywords, source=source)
+        item = NewsItem(title=title, pubtime=pubtime, htmlcontent=htmlcontent, href=href, keywords=keywords, source=source)
         yield item
+
+
+class LetsgojpListSpider(scrapy.Spider):
+    USE_PROXY = True
+    name = "letsgojp_list"
+    allowed_domains = ["letsgojp.com"]
+    start_urls = (
+        "http://okinawa.letsgojp.com/",               # 冲绳
+        "http://hokkaido.letsgojp.com/",              # 北海道
+        "http://tohoku.letsgojp.com/",                # 东北
+        "http://tokyo.letsgojp.com/",                 # 东京
+        "http://hokuriku.letsgojp.com/",              # 北陆
+        "http://kyoto.letsgojp.com/",                 # 京东
+        "http://nara.letsgojp.com/",                  # 奈良
+        "http://kobe.letsgojp.com/",                  # 神户
+        "http://osaka.letsgojp.com/",                 # 大阪
+        "http://wakayama.letsgojp.com/",              # 和歌山
+        "http://kyushu.letsgojp.com/",                # 九州
+    )
+
+    def parse(self, response):
+        hrefs = response.xpath('//a/@href').extract()
+        letsgojp = LetsgojpSpider()
+        r = re.compile("http://.*letsgojp\.com/archives/\d+/$")
+        for href in hrefs:
+            if r.match(href) and not Database.find_dup(href):
+                yield scrapy.Request(href, letsgojp.parse)
